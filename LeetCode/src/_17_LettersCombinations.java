6e70951723a6c72561d486bb05bd56ab3b794f0c
ElvisLiu1994 <719880851@qq.com>
2016-08-30 16:52:53 +0800

    17

--- LeetCode/src/_17_LettersCombinations.java
+++ LeetCode/src/_17_LettersCombinations.java
@@ -1,21 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 
  * @author liujing
  *
  */
 public class _17_LettersCombinations {
-	
+	private static final char[][] map;
 	static {
-		final char[][] map = new char[][]{
-			{}, {}, {'a','b','c'}, {'d','e','f'}, {'g','h','i'},
-		      {'j','k','l'}, {'m','n','o'}, {'p','q','r','s'}, {'t','u','v'},
-		      {'w','x','y','z'}
-		};
+		map = new char[][] { {}, {}, { 'a', 'b', 'c' }, { 'd', 'e', 'f' }, { 'g', 'h', 'i' },
+				{ 'j', 'k', 'l' }, { 'm', 'n', 'o' }, { 'p', 'q', 'r', 's' }, { 't', 'u', 'v' },
+				{ 'w', 'x', 'y', 'z' } };
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static List<String> letterCombinations(String digits) {
+		if (digits == null || digits.length() == 0) {
+			return Arrays.asList();
+		}
+		List<String> res = new ArrayList<String>();
+		res.add("");
+		char[] c = digits.toCharArray();
+		for (int i = 0; i < c.length; i++) {
+			res = combine(map[c[i] - '0'], res);
+		}
+		return res;
+	}
 
+	public static List<String> combine(char[] m, List<String> l){
+		List<String> res = new ArrayList<String>();
+		for(int i = 0; i < m.length; i++){
+			for(String x : l){
+				res.add(x+m[i]);
+			}
+		}
+		return res;
+	}
+	public static void main(String[] args) {
+		
+		System.out.println(letterCombinations("23"));
 	}
 
 }
