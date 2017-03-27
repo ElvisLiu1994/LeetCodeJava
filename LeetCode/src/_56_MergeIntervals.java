2bda95dcac322b891fd5b3ead7fc550e12611296
ElvisLiu1994 <719880851@qq.com>
2017-03-27 23:23:43 +0800

    Merge Intervals

--- LeetCode/src/_56_MergeIntervals.java
+++ LeetCode/src/_56_MergeIntervals.java
@@ -9,9 +9,7 @@ Given a collection of intervals, merge all overlapping intervals.
 For example,
 Given [1,3],[2,6],[8,10],[15,18],
 return [1,6],[8,10],[15,18].
- * @author liujing
- *
- */
+*/
 public class _56_MergeIntervals {
 	
     public List<Interval> merge(List<Interval> intervals) {
