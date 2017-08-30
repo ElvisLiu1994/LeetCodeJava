f2f74983a8e143c2c14df1972355acff95abc66e
ElvisLiu1994 <719880851@qq.com>
2017-08-30 10:17:43 +0800

    77

---
+++ LeetCode/src/_77_Combinations.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+ *
+ */
+public class _77_Combinations {
+	
+    public List<List<Integer>> combine(int n, int k) {
+    	
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+        return res;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
