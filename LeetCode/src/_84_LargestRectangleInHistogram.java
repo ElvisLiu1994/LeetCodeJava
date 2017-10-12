ceeb74251eb276c0029eeb3fc4c82b99546e1e28
ElvisLiu <719880851@qq.com>
2017-10-12 17:50:56 +0800

    84

--- LeetCode/src/_84_LargestRectangleInHistogram.java
+++ LeetCode/src/_84_LargestRectangleInHistogram.java
@@ -30,12 +30,38 @@ public class _84_LargestRectangleInHistogram {
 	}
 	
 	/**
-	 * 
+	 * O(n) 使用栈结构
+	 * 当高度增高时，将该height的index入栈，直到找到某个height比栈顶的height要小，由于
+	 * 栈里面的高度，只能与比它们高的条来组成矩形，所以要将栈里面比当前条形高的条形分别计算
+	 * 面积后出栈，出栈结束的条件是，当前条形高度不再小于栈顶的高度。
 	 */
-//	public int largestRectangleArea1(int[] heights){
-//		if(heights == null || heights.length == 0) return 0;
-//		
-//	}
+	public int largestRectangleArea1(int[] heights){
+		int len = heights.length;
+		
+		int top = 0; //栈顶指针
+		int[] stack = new int[len+1]; //栈数组
+		int maxArea = 0;
+		
+		//为了避免当栈不空时结束循环，假设在heights数组里面有一个高度为0的元素，
+		//这样可以把栈里面所有的元素都出栈，计算出所有可能的矩形面积。循环条件所以也应该是i<=len。
+		for(int i = 0; i <= len; i++){
+			//当前条形的高度，如果i==len，设置高度为0
+			int curHeight = (i == len) ? 0 : heights[i];
+			
+			//如果当前栈为空，或者当前条形高度不低于栈顶的高度，则入栈当前条的index
+			if(top == 0 || curHeight >= heights[stack[top-1]]){
+				stack[top++] = i;
+			}else{//否则进行出栈直至栈顶不高于当前条形，出栈的同时计算面积
+				int stackTopHeight = heights[stack[--top]];
+				//当top==0时说明栈为空，说明栈里面最后的这个元素为前i个(0~i-1)元素中最低的条形
+				//因为比它高的都会出栈，所以这里可以将宽度设为i（0~i-1）。
+				maxArea = Math.max(maxArea, stackTopHeight * ((top == 0) ? i : (i - 1 - stack[top-1])));
+				i--;//此时应该保持当前条形不变，由于for语句中会执行i++,所以这里要先将i减1
+			}
+		}
+		
+		return maxArea;
+	}
 
 	public static void main(String[] args) {
 		_84_LargestRectangleInHistogram test = new _84_LargestRectangleInHistogram();
