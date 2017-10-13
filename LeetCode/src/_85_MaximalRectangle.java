ca2dacf36b6f1df3d09bf7019a9405f90f9537f8
ElvisLiu-windows <719880851@qq.com>
2017-10-13 16:53:52 +0800

    85

---
+++ LeetCode/src/_85_MaximalRectangle.java
@@ -0,0 +1,31 @@
+/**
+Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle 
+containing only 1's and return its area.
+
+For example, given the following matrix:
+
+1 0 1 0 0
+1 0 1 1 1
+1 1 1 1 1
+1 0 0 1 0
+Return 6.
+ *
+ */
+public class _85_MaximalRectangle {
+	
+	public int maximalRectangle(char[][] matrix) {
+		
+		if(matrix == null || matrix.length == 0)	return 0;
+		
+		int rows = matrix.length;
+		int cols = matrix[0].length;
+		
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
