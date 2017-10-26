a965e3ea5ea8b74bdc2fd21dfecaa2ee6c664506
ElvisLiu <719880851@qq.com>
2017-10-27 00:09:19 +0800

    93

---
+++ LeetCode/src/_93_RestoreIPAddress.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+/**
+Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+
+For example:
+Given "25525511135",
+
+return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+ *
+ */
+public class _93_RestoreIPAddress {
+	
+	public static List<String> restoreIpAddresses(String s){
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
