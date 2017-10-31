9acc95e6b1e162f1b75bcc3e2c5c7a4651a8e851
ElvisLiu <719880851@qq.com>
2017-10-31 23:21:59 +0800

    94

---
+++ LeetCode/src/_94_BinaryTreeInorderTraversal.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Given a binary tree, return the inorder traversal of its nodes' values.
+ * 
+ * For example: Given binary tree [1,null,2,3], 1 \ 2 / 3 return [1,3,2].
+ * 
+ * Note: Recursive solution is trivial, could you do it iteratively?
+ *
+ */
+public class _94_BinaryTreeInorderTraversal {
+
+	public static List<Integer> inorderTraversal(TreeNode root) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+
+	TreeNode(int x) {
+		val = x;
+	}
+}
