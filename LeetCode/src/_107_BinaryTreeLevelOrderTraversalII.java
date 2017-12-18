c584f721cf86b7efc92b94a948fd733ad117c50c
ElvisLiu <719880851@qq.com>
2017-12-18 09:18:12 +0800

    107

--- LeetCode/src/_107_BinaryTreeLevelOrderTraversalII.java
+++ LeetCode/src/_107_BinaryTreeLevelOrderTraversalII.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
 Given a binary tree, return the bottom-up level order traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
 
@@ -17,6 +21,32 @@ return its bottom-up level order traversal as:
  *
  */
 public class _107_BinaryTreeLevelOrderTraversalII {
+	
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+        if(root == null) return res;
+        
+        LinkedList<TreeNode> queue = new LinkedList<TreeNode>();
+        
+        queue.offer(root);
+        
+        while(!queue.isEmpty()) {
+        		int levelNum = queue.size();
+        		List<Integer> sublist = new ArrayList<Integer>();
+        		
+        		for(int i = 0; i < levelNum; i++) {
+    				TreeNode tmp = queue.poll();
+    				sublist.add(tmp.val);
+    				if(tmp.left != null) queue.offer(tmp.left);
+    				if(tmp.right != null) queue.offer(tmp.right);
+        		}
+        		
+        		res.add(0, sublist);
+        }
+        
+        return res;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
